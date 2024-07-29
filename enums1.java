09a34c9f83eacad6a0e467e6df92e322390f48a1
atanda0x <atandanafiu21@gmail.com>
2024-07-29 15:39:38 -0700

    enums

---
+++ enums1.java
@@ -0,0 +1,35 @@
+public class enums1 {
+    public static void main(String[] args) {
+        Status s = Status.Pending;
+
+
+        switch (s) {
+            case Running:
+                System.out.println("All good");
+                break;
+            case Failed:
+                System.out.println("Try again");
+                break;
+            case Pending:
+                System.out.println("Please wait");
+                break;
+            default:
+                System.out.println("Done");
+                break;
+        }
+
+        if (s == Status.Running) {
+            System.out.println("All good");
+        } else if (s == Status.Failed) {
+            System.out.println("Try again");
+        }else if (s == Status.Pending) {
+            System.out.println("please wait");
+        }else {
+            System.out.println("Done");
+        }
+    }
+}
+
+enum Status {
+    Running, Failed, Pending, Success;
+}
